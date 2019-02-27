56424b49205f45ceabb0f6367fb929d57556bbee
Jay Ohms <jay.ohms@gmail.com>
2019-02-27 16:14:45 -0600

    Separate the the attach/detach logic of MeFragment into a separate NativeFragment

--- demoapp/src/main/kotlin/com/basecamp/turbolinks/demo/MeFragment.kt
+++ demoapp/src/main/kotlin/com/basecamp/turbolinks/demo/MeFragment.kt
@@ -1,30 +1,16 @@
 package com.basecamp.turbolinks.demo
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.basecamp.turbolinks.TurbolinksActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.bumptech.glide.request.RequestOptions
 import kotlinx.android.synthetic.main.fragment_me.*
 import kotlinx.android.synthetic.main.fragment_me.view.*
 
-class MeFragment : Fragment() {
-    private var listener: TurbolinksActivity? = null
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-
-        when (context) {
-            is TurbolinksActivity -> listener = context
-            else -> throw RuntimeException("$context must implement OnFragmentListener")
-        }
-    }
-
+class MeFragment : NativeFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_me, container, false)
     }
@@ -33,11 +19,6 @@ class MeFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         initView()
         loadAvatar()
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
     }
 
     private fun initView() {
