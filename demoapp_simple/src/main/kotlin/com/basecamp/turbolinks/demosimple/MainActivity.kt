f4f9a4bdfff8fca7f4ebe9e64290f3ba781a2b40
Jay Ohms <jay.ohms@gmail.com>
2019-02-27 09:41:58 -0600

    Bring the demo-simple app up to par with the demo app

--- demoapp_simple/src/main/kotlin/com/basecamp/turbolinks/demosimple/MainActivity.kt
+++ demoapp_simple/src/main/kotlin/com/basecamp/turbolinks/demosimple/MainActivity.kt
@@ -1,113 +1,88 @@
 package com.basecamp.turbolinks.demosimple
 
-import android.view.View
-import android.view.ViewGroup
-import androidx.core.view.isInvisible
-import androidx.core.view.isVisible
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavArgument
-import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.transition.ChangeBounds
-import androidx.transition.TransitionManager
 import com.basecamp.turbolinks.TurbolinksActivity
-import com.basecamp.turbolinks.TurbolinksFragment
+import com.basecamp.turbolinks.TurbolinksActivityDelegate
+import com.basecamp.turbolinks.TurbolinksRouter
 import com.basecamp.turbolinks.TurbolinksSession
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.error.view.*
 
-class MainActivity : TurbolinksActivity() {
+class MainActivity : AppCompatActivity(), TurbolinksActivity {
     private val fragmentId = R.id.section_food_nav
     private val session by lazy { TurbolinksSession.getNew(this) }
+    private val controller by lazy { findNavController(fragmentId) }
+    private val view by lazy { layoutInflater.inflate(R.layout.activity_main, null) }
+    private val router by lazy { Router(this) }
+    private val delegate by lazy { TurbolinksActivityDelegate(this) }
 
-    override val listener = object : Listener {
-        override fun onActivityCreated() {
-            initControllerGraph()
-            initWebView()
-            setupToolbar()
-            verifyServerIpAddress(this@MainActivity)
-        }
+    // ----------------------------------------------------------------------------
+    // AppCompatActivity
+    // ----------------------------------------------------------------------------
 
-        override fun onProvideView(): ViewGroup {
-            return layoutInflater.inflate(R.layout.activity_main, null) as ViewGroup
-        }
-
-        override fun onProvideProgressView(location: String): View {
-            return layoutInflater.inflate(R.layout.progress, null)
-        }
-
-        override fun onProvideErrorView(statusCode: Int): View {
-            return layoutInflater.inflate(R.layout.error, null).apply {
-                error_message.text = Error.getMessage(statusCode)
-            }
-        }
-
-        override fun onProvideNavController(): NavController {
-            return activeNavController()
-        }
-
-        override fun onProvideCurrentDestination(): Fragment? {
-            return activeDestination()
-        }
-
-        override fun onProvideNavigationAction(location: String): Int? {
-            return when (Router.getRouteCommand(location)) {
-                RouteCommand.OPEN_EXTERNAL -> {
-                    Router.launchChromeCustomTab(this@MainActivity, location)
-                    null
-                }
-                RouteCommand.NAVIGATE -> {
-                    Router.getRouteAction(location)
-                }
-            }
-        }
-
-        override fun onProvideSession(fragment: TurbolinksFragment): TurbolinksSession {
-            return session
-        }
-
-        override fun onDestinationTitleChanged(title: String) {
-            updateToolbarTitle(title)
-        }
-
-        override fun onDestinationReplaced() {
-            setupToolbar()
-        }
-
-        override fun onNavigatedForward() {
-            setupToolbar()
-        }
-
-        override fun onNavigatedBackward() {
-            setupToolbar()
-        }
-
-        override fun onBackStackCleared() {
-            setupToolbar()
-        }
-
-        override fun onRequestFullscreen() {
-            toggleFullScreen(true)
-        }
-
-        override fun onRequestExitFullscreen() {
-            toggleFullScreen(false)
-        }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(view)
+        initControllerGraph()
+        initWebView()
+        verifyServerIpAddress(this)
     }
 
-    private fun activeNavController(): NavController {
-        return findNavController(R.id.section_food_nav)
+    override fun onSupportNavigateUp(): Boolean {
+        return navigateUp()
     }
 
-    private fun activeDestination(): Fragment? {
+    override fun onBackPressed() {
+        navigateBack()
+    }
+
+    // ----------------------------------------------------------------------------
+    // TurbolinksActivity interface
+    // ----------------------------------------------------------------------------
+
+    override fun onProvideSession(fragment: Fragment): TurbolinksSession {
+        return session
+    }
+
+    override fun onProvideRouter(): TurbolinksRouter {
+        return router
+    }
+
+    override fun onProvideCurrentDestination(): Fragment {
         val host = supportFragmentManager.findFragmentById(fragmentId)
-        return host?.childFragmentManager?.fragments?.lastOrNull()
+        return host?.childFragmentManager?.primaryNavigationFragment ?:
+                throw IllegalStateException("No current destination found")
     }
+
+    override fun onRequestFinish() {
+        finish()
+    }
+
+    override fun navigate(location: String, action: String) {
+        delegate.navigate(location, action)
+    }
+
+    override fun navigateUp(): Boolean {
+        return delegate.navigateUp()
+    }
+
+    override fun navigateBack() {
+        delegate.navigateBack()
+    }
+
+    override fun clearBackStack() {
+        delegate.clearBackStack()
+    }
+
+    // ----------------------------------------------------------------------------
+    // Private
+    // ----------------------------------------------------------------------------
 
     private fun initControllerGraph() {
-        val controller = activeNavController()
+        // Dynamically set the controller graph and start destination,
+        // so we can use a simplified navigation graph.
         val startLocation = NavArgument.Builder().setDefaultValue(Constants.FOOD_URL).build()
 
         controller.graph = controller.navInflater.inflate(R.navigation.nav_graph).apply {
@@ -118,22 +93,5 @@ class MainActivity : TurbolinksActivity() {
 
     private fun initWebView() {
         session.applyWebViewDefaults()
-    }
-
-    private fun setupToolbar() {
-        val controller = activeNavController()
-        setSupportActionBar(toolbar)
-        setupActionBarWithNavController(controller, AppBarConfiguration(controller.graph))
-        supportActionBar?.setDisplayShowTitleEnabled(false)
-        app_bar_logo.isInvisible = !isAtStartDestination()
-    }
-
-    private fun updateToolbarTitle(title: String) {
-        toolbar.title = if (isAtStartDestination()) "" else title
-    }
-
-    private fun toggleFullScreen(enabled: Boolean) {
-        TransitionManager.beginDelayedTransition(view, ChangeBounds().apply { duration = 150 })
-        app_bar.isVisible = !enabled
     }
 }
---
+++ demoapp_simple/src/main/kotlin/com/basecamp/turbolinks/demosimple/WebHomeFragment.kt
@@ -0,0 +1,16 @@
+package com.basecamp.turbolinks.demosimple
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class WebHomeFragment : WebFragment() {
+    override fun onTitleChanged(title: String) {
+        // Do nothing, we don't want to display the title
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_web_home, container, false)
+    }
+}
