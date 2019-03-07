b87f6113a57cfb9ed29f58e2e5089a9b6f0e95ea
Jay Ohms <jay.ohms@gmail.com>
2019-03-07 06:41:58 -0600

    Change the demo apps' IP addresses back to the default placeholder value

--- demoapp/src/main/kotlin/com/basecamp/turbolinks/demo/Constants.kt
+++ demoapp/src/main/kotlin/com/basecamp/turbolinks/demo/Constants.kt
@@ -1,7 +1,7 @@
 package com.basecamp.turbolinks.demo
 
 object Constants {
-    const val IP_ADDRESS = "10.0.1.22"
+    const val IP_ADDRESS = "x.x.x.x"
     const val BASE_URL = "http://$IP_ADDRESS:9292"
     const val FOOD_URL = BASE_URL
     const val ORDERS_URL = "$BASE_URL/orders"
