c758cb46df16cb4d0fc041a2d1dbce04a8a7e94c
Dan Kim <dan@basecamp.com>
2020-10-14 10:54:51 -0500

    Internalize a couple methods in TurbolinksActivityDelegate. Remove unused resetSessions()

--- turbolinks/src/main/kotlin/com/basecamp/turbolinks/delegates/TurbolinksActivityDelegate.kt
+++ turbolinks/src/main/kotlin/com/basecamp/turbolinks/delegates/TurbolinksActivityDelegate.kt
@@ -6,24 +6,39 @@ import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.basecamp.turbolinks.nav.TurbolinksNavDestination
-import com.basecamp.turbolinks.visit.TurbolinksVisitOptions
 import com.basecamp.turbolinks.session.TurbolinksSessionNavHostFragment
+import com.basecamp.turbolinks.visit.TurbolinksVisitOptions
 
+/**
+ * Provides all the hooks for an activity to communicate with Turbolinks (and vice versa).
+ *
+ * @property activity The activity to bind this delegate to.
+ * @property currentNavHostFragmentId The resource ID of the view bound to the nav host fragment.
+ * @constructor Create empty Turbolinks activity delegate
+ */
 @Suppress("unused", "MemberVisibilityCanBePrivate")
-class TurbolinksActivityDelegate(val activity: AppCompatActivity,
-                                 var currentNavHostFragmentId: Int) {
+class TurbolinksActivityDelegate(
+    val activity: AppCompatActivity,
+    var currentNavHostFragmentId: Int
+) {
 
     private val navHostFragments = mutableMapOf<Int, TurbolinksSessionNavHostFragment>()
 
+    /**
+     * A reference to the session's nav host fragment.
+     */
     val currentSessionNavHostFragment: TurbolinksSessionNavHostFragment
         get() = navHostFragment(currentNavHostFragmentId)
 
+    /**
+     * A reference to the current destination.
+     */
     val currentNavDestination: TurbolinksNavDestination?
         get() = currentFragment as TurbolinksNavDestination?
 
-    /*
-     * Initialize the Activity with a BackPressedDispatcher that
-     * properly handles Fragment navigation with the back button.
+    /**
+     * Initializes the Activity with a BackPressedDispatcher that properly handles Fragment
+     * navigation with the back button.
      */
     init {
         registerNavHostFragment(currentNavHostFragmentId)
@@ -32,40 +47,61 @@ class TurbolinksActivityDelegate(val activity: AppCompatActivity,
         }
     }
 
-    fun registerNavHostFragment(@IdRes navHostFragmentId: Int): TurbolinksSessionNavHostFragment {
+    internal fun registerNavHostFragment(@IdRes navHostFragmentId: Int): TurbolinksSessionNavHostFragment {
         return findNavHostFragment(navHostFragmentId).also {
             navHostFragments[navHostFragmentId] = it
         }
     }
 
-    fun navHostFragment(@IdRes navHostFragmentId: Int): TurbolinksSessionNavHostFragment {
+    internal fun navHostFragment(@IdRes navHostFragmentId: Int): TurbolinksSessionNavHostFragment {
         return requireNotNull(navHostFragments[navHostFragmentId]) {
             "No registered TurbolinksNavHostFragment found"
         }
     }
 
+    /**
+     * Resets each nav host fragment via [TurbolinksSessionNavHostFragment.reset].
+     *
+     */
     fun resetNavHostFragments() {
         navHostFragments.forEach { it.value.reset() }
     }
 
-    fun resetSessions() {
-        navHostFragments.forEach { it.value.session.reset() }
-    }
-
-    fun navigate(location: String,
-                 options: TurbolinksVisitOptions = TurbolinksVisitOptions(),
-                 bundle: Bundle? = null) {
+    /**
+     * Navigate
+     *
+     * @param location
+     * @param options
+     * @param bundle
+     */
+    fun navigate(
+        location: String,
+        options: TurbolinksVisitOptions = TurbolinksVisitOptions(),
+        bundle: Bundle? = null
+    ) {
         currentNavDestination?.navigate(location, options, bundle)
     }
 
+    /**
+     * Navigate up
+     *
+     */
     fun navigateUp() {
         currentNavDestination?.navigateUp()
     }
 
+    /**
+     * Navigate back
+     *
+     */
     fun navigateBack() {
         currentNavDestination?.navigateBack()
     }
 
+    /**
+     * Clear back stack
+     *
+     */
     fun clearBackStack() {
         currentNavDestination?.clearBackStack()
     }
