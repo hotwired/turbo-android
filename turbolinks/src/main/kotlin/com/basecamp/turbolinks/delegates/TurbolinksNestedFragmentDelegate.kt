b7761478c678b168b41f777cf40d0ac5c2d3a573
Dan Kim <dan@basecamp.com>
2020-10-14 13:18:18 -0500

    Docs for TurbolinksNestedFragmentDelegate

--- turbolinks/src/main/kotlin/com/basecamp/turbolinks/delegates/TurbolinksNestedFragmentDelegate.kt
+++ turbolinks/src/main/kotlin/com/basecamp/turbolinks/delegates/TurbolinksNestedFragmentDelegate.kt
@@ -4,9 +4,19 @@ import android.os.Bundle
 import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
 import com.basecamp.turbolinks.nav.TurbolinksNavDestination
-import com.basecamp.turbolinks.visit.TurbolinksVisitOptions
 import com.basecamp.turbolinks.session.TurbolinksSessionNavHostFragment
+import com.basecamp.turbolinks.visit.TurbolinksVisitOptions
 
+/**
+ * A simplified delegate that can be used when only part of the view is delegated to Turbolinks
+ * instead of the entire view being swapped. Example: a search screen with a search bar at the top
+ * that doesn't navigate away, but search results load in a section of the view below the search bar.
+ *
+ * @property fragment The fragment to bind this delegate to.
+ * @constructor
+ *
+ * @param navHostFragmentId
+ */
 @Suppress("unused", "MemberVisibilityCanBePrivate")
 class TurbolinksNestedFragmentDelegate(val fragment: Fragment, navHostFragmentId: Int) {
     val navHostFragment by lazy { findNavHostFragment(navHostFragmentId) }
@@ -14,28 +24,57 @@ class TurbolinksNestedFragmentDelegate(val fragment: Fragment, navHostFragmentId
     val currentNavDestination: TurbolinksNavDestination
         get() = currentFragment as TurbolinksNavDestination
 
+    /**
+     * Resets the nav host fragment via [TurbolinksSessionNavHostFragment.reset]
+     *
+     */
     fun resetNavHostFragment() {
         navHostFragment.reset()
     }
 
+    /**
+     * Resets the Turbolinks session associated with the nav host fragment.
+     *
+     */
     fun resetSession() {
         navHostFragment.session.reset()
     }
 
-    fun navigate(location: String,
-                 options: TurbolinksVisitOptions = TurbolinksVisitOptions(),
-                 bundle: Bundle? = null) {
+    /**
+     * Navigates to the specified location using the current destination as the starting point.
+     *
+     * @param location The location to navigate to.
+     * @param options Any options to apply to the visit.
+     * @param bundle Any additional bundled data to pass to the navigation components.
+     */
+    fun navigate(
+        location: String,
+        options: TurbolinksVisitOptions = TurbolinksVisitOptions(),
+        bundle: Bundle? = null
+    ) {
         currentNavDestination.navigate(location, options, bundle)
     }
 
+    /**
+     * Navigates up using the current destination as the starting point.
+     *
+     */
     fun navigateUp() {
         currentNavDestination.navigateUp()
     }
 
+    /**
+     * Navigates back using the current destination as the starting point.
+     *
+     */
     fun navigateBack() {
         currentNavDestination.navigateBack()
     }
 
+    /**
+     * Clears the nav back stack.
+     *
+     */
     fun clearBackStack() {
         currentNavDestination.clearBackStack()
     }
