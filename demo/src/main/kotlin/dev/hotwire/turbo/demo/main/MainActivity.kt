3dc3958199f717c820a1004ebd3282b9e4c6ea10
Jay Ohms <jay.ohms@gmail.com>
2020-12-10 16:12:33 -0500

    Remove the more complex demo app in favor of the simpler (one session) for now

---
+++ demo/src/main/kotlin/dev/hotwire/turbo/demo/main/MainActivity.kt
@@ -0,0 +1,20 @@
+package dev.hotwire.turbo.demo.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dev.hotwire.turbo.activities.TurboActivity
+import dev.hotwire.turbo.delegates.TurboActivityDelegate
+import dev.hotwire.turbo.demo.R
+import dev.hotwire.turbo.demo.util.verifyServerIpAddress
+
+class MainActivity : AppCompatActivity(), TurboActivity {
+    override lateinit var delegate: TurboActivityDelegate
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        delegate = TurboActivityDelegate(this, R.id.main_nav_host)
+        verifyServerIpAddress(this)
+    }
+}
---
+++ demo/src/main/kotlin/dev/hotwire/turbo/demo/main/MainSessionNavHostFragment.kt
@@ -0,0 +1,40 @@
+package dev.hotwire.turbo.demo.main
+
+import android.app.Activity
+import androidx.fragment.app.Fragment
+import dev.hotwire.turbo.BuildConfig
+import dev.hotwire.turbo.config.TurboPathConfiguration
+import dev.hotwire.turbo.demo.features.imageviewer.ImageViewerFragment
+import dev.hotwire.turbo.demo.features.web.WebFragment
+import dev.hotwire.turbo.demo.features.web.WebHomeFragment
+import dev.hotwire.turbo.demo.util.Constants
+import dev.hotwire.turbo.session.TurboSessionNavHostFragment
+import kotlin.reflect.KClass
+
+@Suppress("unused")
+class MainSessionNavHostFragment : TurboSessionNavHostFragment() {
+    override val sessionName = "main"
+
+    override val startLocation
+        get() = Constants.FOOD_URL
+
+    override val registeredActivities: List<KClass<out Activity>>
+        get() = listOf()
+
+    override val registeredFragments: List<KClass<out Fragment>>
+        get() = listOf(
+            WebFragment::class,
+            WebHomeFragment::class,
+            ImageViewerFragment::class
+        )
+
+    override val pathConfigurationLocation: TurboPathConfiguration.Location
+        get() = TurboPathConfiguration.Location(
+            assetFilePath = "json/configuration.json"
+        )
+
+    override fun onSessionCreated() {
+        super.onSessionCreated()
+        session.setDebugLoggingEnabled(BuildConfig.DEBUG)
+    }
+}
