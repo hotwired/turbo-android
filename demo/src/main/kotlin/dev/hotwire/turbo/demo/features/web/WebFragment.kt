7f038c20562730eafdaa5871c698e6b2e1bedeee
Jay Ohms <jay.ohms@gmail.com>
2020-12-11 15:34:40 -0500

    Handle intercepting a 401 and navigate to signin screen

--- demo/src/main/kotlin/dev/hotwire/turbo/demo/features/web/WebFragment.kt
+++ demo/src/main/kotlin/dev/hotwire/turbo/demo/features/web/WebFragment.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.widget.Toolbar
 import dev.hotwire.turbo.demo.base.NavDestination
 import dev.hotwire.turbo.demo.R
 import dev.hotwire.turbo.demo.util.Error
+import dev.hotwire.turbo.demo.util.SIGN_IN_URL
 import dev.hotwire.turbo.fragments.TurboWebFragment
 import dev.hotwire.turbo.nav.TurboNavGraphDestination
 import dev.hotwire.turbo.views.TurboView
@@ -43,4 +44,11 @@ open class WebFragment : TurboWebFragment(), NavDestination {
 
     override val turboView: TurboView?
         get() = view?.findViewById(R.id.turbo_view)
+
+    override fun onVisitErrorReceived(location: String, errorCode: Int) {
+        when (errorCode) {
+            401 -> navigate(SIGN_IN_URL)
+            else -> super.onVisitErrorReceived(location, errorCode)
+        }
+    }
 }
