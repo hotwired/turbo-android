8216fa830a2f23e7979e17f10996bf56c6bb4185
Jay Ohms <jay.ohms@gmail.com>
2020-12-17 15:47:18 -0500

    Document the Activity/Delegate

--- turbo/src/main/kotlin/dev/hotwire/turbo/activities/TurboActivity.kt
+++ turbo/src/main/kotlin/dev/hotwire/turbo/activities/TurboActivity.kt
@@ -1,12 +1,12 @@
 package dev.hotwire.turbo.activities
 
 import dev.hotwire.turbo.delegates.TurboActivityDelegate
+import dev.hotwire.turbo.session.TurboSessionNavHostFragment
 
 /**
- * Interface that should be implemented by any activity using Turbo. Ensures that the
- * activity provides a [TurboActivityDelegate] so the framework can communicate back and forth.
- *
- * @constructor Create empty Turbo activity
+ * Interface that should be implemented by any Activity using Turbo. Ensures that the
+ * Activity provides a [TurboActivityDelegate] so the framework can initialize the
+ * [TurboSessionNavHostFragment] hosted in your Activity's layout resource.
  */
 interface TurboActivity {
     var delegate: TurboActivityDelegate
