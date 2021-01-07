c28f7df6e45fb4ff9aa1ee1f9e59fc869920c393
Jay Ohms <jay.ohms@gmail.com>
2021-01-07 13:51:14 -0500

    Use the TestCoroutineDispatcher to run the TurboFileUploadDelegate tests

--- turbo/src/test/kotlin/dev/hotwire/turbo/delegates/TurboFileUploadDelegateTest.kt
+++ turbo/src/test/kotlin/dev/hotwire/turbo/delegates/TurboFileUploadDelegateTest.kt
@@ -1,49 +1,50 @@
 package dev.hotwire.turbo.delegates
 
 import android.app.Activity
+import android.content.Context
 import android.os.Build
-import com.nhaarman.mockito_kotlin.whenever
-import dev.hotwire.turbo.nav.TurboNavDestination
+import androidx.test.core.app.ApplicationProvider
+import dev.hotwire.turbo.BaseRepositoryTest
 import dev.hotwire.turbo.session.TurboSession
 import dev.hotwire.turbo.util.TurboFileProvider
-import dev.hotwire.turbo.util.toJson
 import dev.hotwire.turbo.views.TurboWebView
-import dev.hotwire.turbo.visit.TurboVisit
-import dev.hotwire.turbo.visit.TurboVisitOptions
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
-import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations
 import org.robolectric.Robolectric.buildActivity
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 import java.io.File
 
+@ExperimentalCoroutinesApi
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [Build.VERSION_CODES.O])
-class TurboFileUploadDelegateTest {
+class TurboFileUploadDelegateTest : BaseRepositoryTest() {
     @Mock
     private lateinit var webView: TurboWebView
     private lateinit var activity: Activity
+    private lateinit var context: Context
     private lateinit var session: TurboSession
 
     @Before
-    fun setup() {
+    override fun setup() {
         MockitoAnnotations.openMocks(this)
 
         activity = buildActivity(TurboTestActivity::class.java).get()
+        context = ApplicationProvider.getApplicationContext()
         session = TurboSession("test", activity, webView)
         session.fileUploadDelegate.coroutineDispatcher = Dispatchers.Main
     }
 
     @Test
     fun fileProviderDirectoryIsCleared() {
-        val dir = TurboFileProvider.directory(activity)
+        val dir = TurboFileProvider.directory(context)
 
         File(dir, "testFile.txt").apply {
             writeText("text")
