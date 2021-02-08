95ee001c4c1c3f4a5ff9b4b135da3a729a4a9283
Jay Ohms <jay.ohms@gmail.com>
2021-02-08 10:11:58 -0500

    Allow target="_blank" links to propose visits, even when embedded in <iframe> content

--- turbo/src/main/kotlin/dev/hotwire/turbo/views/TurboWebChromeClient.kt
+++ turbo/src/main/kotlin/dev/hotwire/turbo/views/TurboWebChromeClient.kt
@@ -1,10 +1,13 @@
 package dev.hotwire.turbo.views
 
 import android.net.Uri
+import android.os.Message
 import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import dev.hotwire.turbo.session.TurboSession
+import dev.hotwire.turbo.util.toJson
+import dev.hotwire.turbo.visit.TurboVisitOptions
 
 open class TurboWebChromeClient(val session: TurboSession) : WebChromeClient() {
     override fun onShowFileChooser(
@@ -16,5 +19,19 @@ open class TurboWebChromeClient(val session: TurboSession) : WebChromeClient() {
             filePathCallback = filePathCallback,
             params = fileChooserParams
         )
+    }
+
+    override fun onCreateWindow(webView: WebView, isDialog: Boolean, isUserGesture: Boolean, resultMsg: Message?): Boolean {
+        val message = webView.handler.obtainMessage()
+        webView.requestFocusNodeHref(message)
+
+        message.data.getString("url")?.let {
+            session.visitProposedToLocation(
+                location = it,
+                optionsJson = TurboVisitOptions().toJson()
+            )
+        }
+
+        return false
     }
 }
