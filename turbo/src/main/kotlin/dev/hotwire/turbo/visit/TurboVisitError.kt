11bf8738fffc693ca4a7d6143fcde10e83d13315
Jay Ohms <jay.ohms@gmail.com>
2024-02-26 12:56:20 -0500

    Add API documentation for the error properties and types

--- turbo/src/main/kotlin/dev/hotwire/turbo/visit/TurboVisitError.kt
+++ turbo/src/main/kotlin/dev/hotwire/turbo/visit/TurboVisitError.kt
@@ -2,17 +2,17 @@ package dev.hotwire.turbo.visit
 
 data class TurboVisitError(
     /**
-     *
+     * The [TurboVisitErrorType] type of error received.
      */
     val type: TurboVisitErrorType,
 
     /**
-     *
+     * The error code associated with the [TurboVisitErrorType] type.
      */
     val code: Int,
 
     /**
-     * 
+     * The (optional) description of the error.
      */
     val description: String? = null
 )
